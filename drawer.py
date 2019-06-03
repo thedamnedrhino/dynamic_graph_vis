885c06c9b4ca124338d2fc776f67d796a1070122
fsbakhtiar <farzadsbakhtiar@gmail.com>
2019-06-03 15:28:20 -0700

    modified drawer py. Its basic dynamic functionality is operational at this point

--- drawer.py
+++ drawer.py
@@ -28,54 +28,31 @@ for i in range(num_plots):
     pylab.draw()
     pause(2)
 """
-
-class Drawer:
-	def __init__(self):
-		self.networkx_edges = None
-	def draw(self, graph):
-		g = nx.DiGraph()
-		g.add_nodes_from([node.l for node in graph.get_nodes()])
-		g.add_edges_from([(e.u.l, e.v.l) for e in graph.get_edges()])
-		nx.draw(g)
-	def update(self, old_graph, new_graph, adds, removes):
-		# color the removed edges white
-		nx.draw_networkx_edges()
-
 class DynamicDrawer:
 	def __init__(self, base_graph):
 		self.graph = base_graph
 		self.nx_graph = nx.complete_graph([node.l for node in self.graph.get_nodes()])
 		self.pos = nx.spring_layout(self.nx_graph)
+		self.edge_color = 'b'
 
 	def start(self):
 		nx.draw_networkx_nodes(self.nx_graph, self.pos)
 		nx.draw_networkx_edges(self.nx_graph, self.pos, edgelist=self.nx_graph.edges(), edge_color='w')
 		edgelist = [(e.u.l, e.v.l) for e in self.graph.get_edges()]
-		nx.draw_networkx_edges(self.nx_graph, self.pos, edgelist=edgelist, edge_color='b')
-
-
-def test():
-	pylab.ion()
-	d = Drawer()
-	pylab.show()
-	g = graph.get_test_graph()
-	g.remove_edge(1, 2)
-	d.draw(g)
-	# plt.show()
-	pylab.draw()
-	pause(2)
-	g = graph.get_test_graph()
-	plt.clf()
-	d.draw(g)
-	# plt.show()
-	pylab.draw()
-	pause(2)
+		nx.draw_networkx_edges(self.nx_graph, self.pos, edgelist=edgelist, edge_color=self.edge_color)
 
 def test1():
+	pylab.ion()
+	pylab.show()
 	g = graph.get_test_graph()
 	d = DynamicDrawer(g)
 	d.start()
-	plt.show()
+	pylab.draw()
+	plt.waitforbuttonpress()
+	d.edge_color = 'y'
+	d.start()
+	pylab.draw()
+	plt.waitforbuttonpress()
 
 
 
